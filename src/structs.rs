1c5b7c5339376d14e26a7281286c0bf0154f60ad
Erwan Egasse <contact@erwan.sh>
2023-11-05 12:45:27 +0100

    structs: switch to standard enum

--- src/structs.rs
+++ src/structs.rs
@@ -1,21 +1,33 @@
-// STRUCTS
+#![allow(non_camel_case_types)]
 
-#[non_exhaustive]
-pub struct WeaponType;
-impl WeaponType
+// -----------------------------
+// ENUMS
+// -----------------------------
+
+pub enum WeaponType
 {
-    pub const SHOVEL:            &str = "Shovel";
-    pub const BASEBALL_BAT:      &str = "Baseball Bat";
-    pub const SWORD:             &str = "Sword";
-    pub const AMERICAN_KNUCKLES: &str = "American Knuckles";
-    pub const COOKING_PAN:       &str = "Cooking Pan";
-    pub const CHAIR:             &str = "Chair";
-    pub const AXE:               &str = "Axe";
-    pub const TELESCOPIC_BATON:  &str = "Telescopic Baton";
-    pub const STATS_TEXTBOOK:    &str = "Stats Textbook";
-    pub const DRUM_STICKS:       &str = "Drum Sticks";
+    SHOVEL,
+    BASEBALL_BAT,
+    SWORD,
+    AMERICAN_KNUCKLES,
+    COOKING_PAN,
+    CHAIR,
+    AXE,
+    TELESCOPIC_BATON,
+    STATS_TEXTBOOK,
+    DRUM_STICKS,
 }
 
+pub enum HealthType
+{
+    POTION,
+    BAND_AID,
+    FOOD,
+}
+
+// -----------------------------
+// STRUCTS
+// -----------------------------
 
 pub struct Player
 {
@@ -50,23 +62,6 @@ pub struct Clothes
     pub style_score: f32,
 }
 
-
-enum HealthType
-{
-    POTION,
-    BAND_AID,
-    FOOD,
-}
-
-// #[non_exhaustive]
-// pub struct HealthType;
-// impl HealthType
-// {
-//     pub const FOOD:     &str = "Food";
-//     pub const BAND_AID: &str = "Band-aid";
-//     pub const POTION:   &str = "Potion";
-// }
-
 pub struct Health
 {
     pub health_type: HealthType,
@@ -86,8 +81,8 @@ pub struct Enemy
 
 pub struct Room
 {
-    pub enemies: Option<Vec<Enemy>>,
-    pub clerk:   Option<Clerk>,
+    pub enemies:       Option<Vec<Enemy>>,
+    pub clerk:         Option<Clerk>,
     pub previous_room: Option<String>,
     pub next_room:     String,
 }
